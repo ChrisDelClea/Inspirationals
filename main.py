f6879bbf73ee57b5d83677972b6e846e2b9e2373
Christian Klose <chris.klose@gmx.net>
2020-11-25 10:52:43 +0100

    upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from SPARQLWrapper import SPARQLWrapper, JSON
-from streamlit_agraph import agraph, TripleStore, Config
+from streamlit_agraph import agraph, TripleStore, Node, Edge, Config
 from layout import footer
+import json
 
 def get_inspired():
   sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -56,9 +57,10 @@ def app():
   footer()
   st.title("Graph Example")
   st.sidebar.title("Welcome")
-  query_type = st.sidebar.selectbox("Quey Tpye: ", ["Inspirationals"]) # could add more stuff here later on or add other endpoints in the sidebar.
+  query_type = st.sidebar.selectbox("Query Tpye: ", ["Inspirationals", "Marvel"]) # could add more stuff here later on or add other endpoints in the sidebar.
   config = Config(height=500, width=700, nodeHighlightBehavior=True, highlightColor="#F7A7A6", directed=True,
                   collapsible=True)
+
   if query_type=="Inspirationals":
     st.subheader("Inspirationals")
     with st.spinner("Loading data"):
@@ -66,6 +68,21 @@ def app():
       st.write("Nodes loaded: " + str(len(store.getNodes())))
     st.success("Done")
     agraph(list(store.getNodes()), (store.getEdges() ), config)
+
+  if query_type=="Marvel":
+    with open("./marvel.json", encoding="utf8") as f:
+      marvel_file = json.loads(f.read())
+      marvel_store = TripleStore()
+      for sub_graph in marvel_file["children"]:
+        marvel_store.add_triple(marvel_file["name"], "has_subgroup", sub_graph["name"], picture=marvel_file["img"])
+        for node in sub_graph["children"]:
+          node1 = node["hero"]
+          link = "blongs_to"
+          node2 = sub_graph["name"]
+          pic = node["img"]
+          marvel_store.add_triple(node1, link, node2, picture=pic)
+      agraph(list(marvel_store.getNodes()), (marvel_store.getEdges()), config)
+
 
 if __name__ == '__main__':
     app()
